cab72fb027f86f814e6b21d33adbfa50d02e6bb3
rodolfo.smneto <rodolfo.smneto@CAS555094W0735.senacsp.edu.br>
2016-08-19 20:32:20 -0300

    terceiro commit

--- agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -12,7 +12,7 @@ package br.senac.tads3.agenda;
 public class Agenda {
     
      public static void main(String[] args) {
-       System.out.println("!!!!!!!!!");
+       System.out.println("!!!!!!!!!**********");
     }
     
 }
